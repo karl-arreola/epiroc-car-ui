b0a1a146bb3c2c63324dea9160de9256211e4963
karreola-vonclaro <karreola@vonclaro.com>
2024-12-04 17:06:53 -0800

    init ui

--- app/page.js
+++ app/page.js
@@ -1,101 +1,122 @@
-import Image from "next/image";
+"use client";
+
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import BuildIcon from '@mui/icons-material/Build';
+import Battery20Icon from '@mui/icons-material/Battery20';
+import SystemSecurityUpdateWarningIcon from '@mui/icons-material/SystemSecurityUpdateWarning';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import PowerIcon from '@mui/icons-material/Power';
+import Slider from '@mui/material/Slider';
+import GaugeComponent from 'react-gauge-component';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const kW = (value) => {
+    return value + ' kW';
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const rpm = (value) => {
+    return value + ' RPM';
+  }
+
+  return (
+    <div className="main-container">
+      <div className="car-ui-container">
+        <div className="top-container">
+          <div className="parking indicator"><LocalParkingIcon/></div>
+          <div className="check-engine indicator"><BuildIcon/></div>
+          <div className="motor-status indicator"><SystemSecurityUpdateWarningIcon/></div>
+          <div className="battery-low indicator"><Battery20Icon/></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="guages-container">
+          <GaugeComponent
+            arc={{
+              nbSubArcs: 150,
+              colorArray: ['#5BE12C', '#F5CD19', '#EA4228'],
+              width: 0.3,
+              padding: 0.003
+            }}
+            labels={{
+              valueLabel: {
+                style: {fontSize: 40},
+                formatTextValue: kW
+              },
+              tickLabels: {
+                type: "outer",
+                ticks: [
+                  { value: -1000 },
+                  { value: -750 },
+                  { value: -500 },
+                  { value: -250},
+                  { value: 0 },
+                  { value: 250 },
+                  { value: 500 },
+                  { value: 750 },
+                  { value: 1000 },
+                ],
+                defaultTickValueConfig: {
+                  formatTextValue: kW
+                }
+              }
+            }}
+            value={0}
+            minValue={-1000}
+            maxValue={1000}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <GaugeComponent
+            arc={{
+              nbSubArcs: 150,
+              colorArray: ['#5BE12C', '#F5CD19', '#EA4228'],
+              width: 0.3,
+              padding: 0.003
+            }}
+            labels={{
+              valueLabel: {
+                style: {fontSize: 40},
+                formatTextValue: rpm
+              },
+              tickLabels: {
+                type: "outer",
+                ticks: [
+                  { value: 0 },
+                  { value: 100 },
+                  { value: 200 },
+                  { value: 300},
+                  { value: 400 },
+                  { value: 500 },
+                  { value: 600 },
+                  { value: 700 },
+                  { value: 800 },
+                ],
+                defaultTickValueConfig: {
+                  formatTextValue: rpm
+                }
+              }
+            }}
+            value={0}
+            maxValue={800}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="middle-container">
+          <div className="gear-ratio meter"><SettingsIcon/></div>
+          <div className="battery meter"><Battery20Icon/></div>
+          <div className="thermostat meter"><ThermostatIcon/></div>
+          <div className="battery-temp meter"><SystemSecurityUpdateWarningIcon/></div>
+          <div className="motor-speed meter">
+            <Slider defaultValue={0} step={1} marks min={0} max={4}/>
+          </div>
+        </div>
+        <div className="bottom-container">
+          <div className="gear-motor-temp btn">
+            <SettingsIcon/>
+            <SystemSecurityUpdateWarningIcon/>
+            <ThermostatIcon/>
+          </div>
+          <div className="view-menu btn"><WidgetsIcon/></div>
+          <div className="charge btn"><PowerIcon/></div>
+        </div>
+      </div>
     </div>
   );
 }
